e8a65a0b4bde80c85c7775decd238aca24f1cedc
Rui Pinheiro <rui.pinheiro@askblue.pt>
2025-01-10 17:09:40 +0000

    Env local and prod config

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,14 +3,14 @@ import { getDatabase, ref, set, get, update } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC9B-D2YC4pJkfEWtohlAU0ojsmQAvnFAo",
-    authDomain: "lies-502a0.firebaseapp.com",
-    databaseURL: "https://lies-502a0-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "lies-502a0",
-    storageBucket: "lies-502a0.firebasestorage.app",
-    messagingSenderId: "572668099775",
-    appId: "1:572668099775:web:7d8f0f5402eb9bba3ada0a",
-    measurementId: "G-RB5GMC0XQC"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
